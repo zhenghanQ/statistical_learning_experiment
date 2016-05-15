6f81e68e4fb5c50570ecb1670ece6c6b817f8710
Yoel Sanchez <Yoel Sanchez Araujo>
2016-05-14 21:36:22 -0400

    testing jspsych

--- experiment.js
+++ experiment.js
@@ -1,13 +1,12 @@
 <script>
-(function() { var rotate = document.getElementById('rotate'); var iDir =
-'images/square.png'; var idx = 0; var imag = ['square.png','circle.png','triangle.png',
-'trapezoid.png'];
-var imagPres = 0; var
-lengthInMilliseconds = 1000; var len = imag.length; var chImg = function
-() { imagPres = 0.2; if (idx % 2 == 0) { imagPres = 0.8;}
-idx = idx === len - 1 ? 0 : ++idx; rotate.src = iDir +
-imag[idx]; setTimeout(chImg, imagPres * lengthInMilliseconds);
-};
-setTimeout(chImg, imagPres * lengthInMilliseconds);
-})();
+
+    var hello_trial = {
+        type: 'text',
+        text: 'Hello World'
+    }
+
+    jsPsych.init({
+        timeline: [ hello_trial ]
+    })
+
 </script> 
