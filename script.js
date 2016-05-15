214b76dc4cf803cc1f2c640e50e45650fb3111f8
Yoel Sanchez <Yoel Sanchez Araujo>
2016-05-14 20:35:30 -0400

    added experiment to tab

--- script.js
+++ script.js
@@ -5,4 +5,5 @@ document.getElementById("nav01").innerHTML =
 "<ul id='menu'>" +
 "<li><a href='index.html'>Home</a></li>" +
 "<li><a href='about.html'>About</a></li>" +
+"<li><a href='experiment.html'>Experiment</a></li>" +
 "</ul>";
