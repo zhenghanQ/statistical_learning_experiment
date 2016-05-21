9f22f560bf6120d6c290b3bb9f3a6fdbdb78d521
YSanchezAraujo <yoelsanchez006@gmail.com>
2016-05-21 19:16:00 -0400

    Update vsl.js

--- vsl.js
+++ vsl.js
@@ -5,6 +5,12 @@ var par_id = {
     questions: ['Please enter your participant ID','question']
 };
  
+String.prototype.format = function () {
+    var i = 0, args = arguments;
+    return this.replace(/{}/g, function () {
+        return typeof args[i] != 'undefined' ? args[i++] : '';
+    });
+};
 
 
 
