283090031e1db4dcdafebbc697d2ba5111fd1930
YSanchezAraujo <yoelsanchez006@gmail.com>
2016-05-21 14:20:14 -0400

    Update vsl.js

--- vsl.js
+++ vsl.js
@@ -16,7 +16,7 @@ String.prototype.format = function () {
 
 function target() {
     jsPsych.data.getLastTrialData();
-    var exp_tar = 'Alien{}.png'.format(data.respones['Q1']);
+    var exp_tar = 'images/Alien{}.png'.format(data.respones['Q1']);
     return exp_tar
 };
 
