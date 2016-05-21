de9f8b9055c836c0a40ca1dc9c5451d9700b644e
YSanchezAraujo <yoelsanchez006@gmail.com>
2016-05-21 14:06:37 -0400

    Update vsl.js

--- vsl.js
+++ vsl.js
@@ -15,12 +15,13 @@ var welcome = {
 };
 
 // function for string formating, helps with target selection 
-String.prototype.format = function() {
+String.prototype.format = function () {
     var i = 0, args = arguments;
     return this.replace(/{}/g, function () {
         return typeof args[i] != 'undefined' ? args[i++] : '';
     });
-}; 
+};
+
 
 var tar = 'Alien{}'.format(exp_tar_cond);
   
