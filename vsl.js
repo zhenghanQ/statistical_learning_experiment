4247f83e88ca2d54915e283931eaf17283e1f858
YSanchezAraujo <yoelsanchez006@gmail.com>
2016-05-27 19:51:15 -0400

    Update vsl.js

--- vsl.js
+++ vsl.js
@@ -67,7 +67,9 @@ var target_alien_reminder = {
 
 
 // the sequence of images to show
-var seq1 = [3,1];
+var seq1 = [3,1,2,1,2,3,2,1,2,1,2,3,1,2,3,3,1,2,1,2,3,2,1,2,1,2,3,1,2,3,
+            3,1,2,1,2,3,2,1,2,1,2,3,1,2,3,3,1,2,1,2,3,2,1,2,1,2,3,1,2,3,
+            3,1,2,1,2,3,2,1,2,1,2,3,1,2,3,3,1,2,1,2,3,2,1,2,1,2,3,1,2,3];
 
 
 // creates a javascript object that will be used in the for loop below
